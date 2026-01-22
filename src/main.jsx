c29dfcae23075ea6dfbf8faba562349ee8445837
Alex Cory Berry <acberry1@uw.edu>
2026-01-21 22:06:20 -0800

    ok

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter basename="/">
+    <BrowserRouter>
         <App />
     </BrowserRouter>
 );
